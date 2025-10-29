489007f3fe1fcc0202ddce40015564b11b1cf52c
viraj992 <it21183836@my.sliit.lk>
2025-10-29 22:55:02 +0530

    Google Login function implemented succefully ✅😊

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { createUser, getUser, loginUser } from '../controllers/userController.js';
+import { createUser, getUser, googleLogin, loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 userRouter.post("/",createUser);
 userRouter.get("/",getUser)
 userRouter.post("/login", loginUser)
-
+userRouter.post("/google-Login",googleLogin)
 
 export default userRouter; //tyt
